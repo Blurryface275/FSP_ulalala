d80ffe29448690892de75b47e5e5cc4b5a68a868
Stephen <stephen.sugianto234@gmail.com>
2025-09-18 08:57:43 +0700

    Login dun

---
+++ login-process.php
@@ -0,0 +1,27 @@
+<?php
+    $mysqli = mysqli_connect("localhost", "root", "", "fullstack");
+
+    if (isset($_POST['login'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    }
+
+    // pengecekan di sini, jadi kalo query ini mengembalikan hasil maka username sm password itu benar
+    $sql="select username, password from akun where username=? and password=?"; 
+    $stmt=$mysqli->prepare($sql); 
+    $stmt->bind_param("ss",$username, $password); //masukin username + password ke parameter
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($row = $result->fetch_assoc()){
+        //Kalo username sm password coock
+        echo "Login berhasil! Selamat datang...";
+    }
+    else{
+        // Tidak cocok username sm passwordnya
+        echo "Username atau password salah!";
+    }
+
+    header("Location: home.php");
+    exit;
+?>
