dbbc5d344a0dabb03a2b22447af0b452a8c06533
nicoleoliviat <s160423157@student.ubaya.ac.id>
2025-09-22 23:19:02 +0700

    udah kubalikin ke normal

--- data-dosen.php
+++ data-dosen.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         .foto {
-            max-width: 15px;
+            max-width: 150px;
 
         }
     </style>
