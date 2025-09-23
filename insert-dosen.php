0b1d3ba202b36f22f54f085290fc7c16f4c9530c
Stephen <stephen.sugianto234@gmail.com>
2025-09-23 11:42:41 +0700

    insert mahasiswa

--- insert-dosen.php
+++ insert-dosen.php
@@ -1,3 +1,38 @@
+<?php
+    $mysqli = new mysqli("localhost", "root", "", "fullstack");
+    if ($mysqli->connect_errno) {
+       die("Failed to connect to MySQL: " . $mysqli->connect_error);
+    }
+
+    if (isset($_POST['insert'])){
+        $nama = $_POST['nama']; // nama dosen
+        $NPK = $_POST['NPK']; // NPK dosen
+    }
+  
+    $foto = $_FILES['foto'];
+    $sql = "insert into dosen (npk, nama, foto_extension) values(?,?,?)";
+
+    $ext = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+    $target = "uploads/" . $NPK . "." . $ext; //kasih nama pakai npk dan extension
+
+    // Pindahkan file ke folder uploads
+    if (move_uploaded_file($_FILES['foto']['tmp_name'], $target)) {
+        // Simpan ke database
+        $sql = "INSERT INTO dosen (npk, nama, foto_extension) VALUES (?, ?, ?)";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("sss", $NPK, $nama, $ext);
+        $stmt->execute();
+
+        echo "Data dosen berhasil disimpan!";
+    } else {
+        echo "Gagal upload foto!";
+    }
+
+    
+    header("Location: data-dosen.php");
+    exit;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +47,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
-<form action="insert-process.php" method="POST" enctype="multipart/form-data">
+<form action="" method="POST" enctype="multipart/form-data">
 
         <p>
             <label for="nama">Nama : </label>
--- insert-mahasiswa.php
+++ insert-mahasiswa.php
@@ -1,3 +1,39 @@
+<?php
+    $mysqli = new mysqli("localhost", "root", "", "fullstack");
+    if ($mysqli->connect_errno) {
+       die("Failed to connect to MySQL: " . $mysqli->connect_error);
+    }
+
+    if (isset($_POST['inserts'])){//ini 'inserts' sesuai yang insert mhs namanya
+        $nama = $_POST['nama']; // nama mhs
+        $NRP = $_POST['NRP']; // nrp
+    }
+  
+    $foto = $_FILES['foto'];
+    $sql = "insert into mahasiswa (nrp, nama, gender, tanggal_lahir, angkatan, foto_extension) values(?,?,?,?,?,?)";
+
+    $ext = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+    $target = "uploads/" . $NRP . "." . $ext; //kasih nama pakai nrp dan extension
+
+    // Pindahkan file ke folder uploads
+    if (move_uploaded_file($_FILES['foto']['tmp_name'], $target)) {
+        // Simpan ke database
+        $sql = "INSERT INTO mahasiswa (nrp, nama, gender, tanggal_lahir, angkatan, foto_extention) VALUES (?,?,?,?,?,?)";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("ssedss", $NPK, $nama, $ext);//ini enum sebutannnya e kah?
+        $stmt->execute();
+
+        echo "Data Mahasiswa berhasil disimpan!";
+        header("Location: data-mahasiswa.php");
+        exit;
+    } else {
+        echo "Gagal upload foto!";
+    }
+
+    
+   
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +48,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
-<form action="insert-process.php" method="POST" enctype="multipart/form-data">
+<form action="" method="POST" enctype="multipart/form-data">
 
         <p>
             <label for="nama">Nama : </label>
@@ -24,17 +60,20 @@
         </p>
           <p>
             <label for="gender">Gender : </label> <!-- ini gimana ya cara bikin option gender??? -->
-            <input type="text" name="gender" id="gender">
+            <label for="pria">Pria</label>
+            <input type="radio" name="pria" id="pria">
+            <label for="wanita">Wanita</label>
+            <input type="radio" name="wanita" id="wanita">
         </p>
 
          <p>
-            <label for="tgl">tanggal_lahir : </label> <!-- ini gimana ya cara bikin kalender??? -->
-            <input type="text" name="tgl" id="tgl">
+            <label for="tgl">Tanggal lahir : </label> <!-- ini gimana ya cara bikin kalender??? -->
+            <input type="date" name="tgl" id="tgl">
         </p>
 
           <p>
-            <label for="angkatan">angkatan : </label> <!-- ini bener kah? -->
-            <input type="text" name="angkatan" id="angkatan">
+            <label for="angkatan">Angkatan : </label> <!-- ini bener kah? -->
+            <input type="number" name="angkatan" id="angkatan">
         </p>
 
         <p>
